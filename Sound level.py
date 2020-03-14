9020b298922e1526a1b14e04595843ec45aee4b0
Reddy123cloud <61586256+Reddy123cloud@users.noreply.github.com>
2020-03-14 10:58:55 -0700

    Update Sound level.py

--- Sound level.py
+++ Sound level.py
@@ -1,13 +1,14 @@
 def survey():
-    avg=0
     L=[]
-    a=int(input("Enter the sound level"))
-    L.append(a)
-    avg+=a
-    while a!=0: 
-        for j in L:
-           if L[j]==0:
-             print("average sound level",int(avg/n))
-             print("highest recorded sound level",max(L))
+    avg=0
+    count=0
+    while True:
+        a=int(input("Enter the sound level"))
+        L.append(a)
+        avg+=a
+        count+=1
+        if a==0:
+           print("average sound level",int(avg/count))
+           print("highest recorded sound level",max(L))
+           break
 survey()
-
